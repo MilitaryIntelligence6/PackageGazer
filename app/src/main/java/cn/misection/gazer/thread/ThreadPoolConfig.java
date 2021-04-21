01bed55cac123f41570273475a9f6a07d0f4663b
MilitaryIntelligence6 <hlrongzun@qq.com>
2021-04-22 00:39:09 +0800

    重构;

--- app/src/main/java/cn/misection/gazer/thread/ThreadPoolConfig.java
+++ app/src/main/java/cn/misection/gazer/thread/ThreadPoolConfig.java
@@ -4,7 +4,9 @@ import android.support.annotation.NonNull;
 
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -24,136 +26,7 @@ class ThreadPoolConfig {
 
     public static final TimeUnit unit = TimeUnit.SECONDS;
 
-    public static final BlockingQueue<Runnable> workQueue = new BlockingQueue<Runnable>() {
-        @Override
-        public boolean add(Runnable runnable) {
-            return false;
-        }
-
-        @Override
-        public boolean offer(Runnable runnable) {
-            return false;
-        }
-
-        @Override
-        public void put(@NonNull Runnable runnable) throws InterruptedException {
-
-        }
-
-        @Override
-        public boolean offer(Runnable runnable, long timeout, @NonNull TimeUnit unit) throws InterruptedException {
-            return false;
-        }
-
-        @NonNull
-        @Override
-        public Runnable take() throws InterruptedException {
-            return null;
-        }
-
-        @Override
-        public Runnable poll(long timeout, @NonNull TimeUnit unit) throws InterruptedException {
-            return null;
-        }
-
-        @Override
-        public int remainingCapacity() {
-            return 0;
-        }
-
-        @Override
-        public boolean remove(Object o) {
-            return false;
-        }
-
-        @Override
-        public boolean contains(Object o) {
-            return false;
-        }
-
-        @Override
-        public int drainTo(@NonNull Collection<? super Runnable> c) {
-            return 0;
-        }
-
-        @Override
-        public int drainTo(@NonNull Collection<? super Runnable> c, int maxElements) {
-            return 0;
-        }
-
-        @Override
-        public Runnable remove() {
-            return null;
-        }
-
-        @Override
-        public Runnable poll() {
-            return null;
-        }
-
-        @Override
-        public Runnable element() {
-            return null;
-        }
-
-        @Override
-        public Runnable peek() {
-            return null;
-        }
-
-        @Override
-        public int size() {
-            return 0;
-        }
-
-        @Override
-        public boolean isEmpty() {
-            return false;
-        }
-
-        @NonNull
-        @Override
-        public Iterator<Runnable> iterator() {
-            return null;
-        }
-
-        @NonNull
-        @Override
-        public Object[] toArray() {
-            return new Object[0];
-        }
-
-        @NonNull
-        @Override
-        public <T> T[] toArray(@NonNull T[] a) {
-            return null;
-        }
-
-        @Override
-        public boolean containsAll(@NonNull Collection<?> c) {
-            return false;
-        }
-
-        @Override
-        public boolean addAll(@NonNull Collection<? extends Runnable> c) {
-            return false;
-        }
-
-        @Override
-        public boolean removeAll(@NonNull Collection<?> c) {
-            return false;
-        }
-
-        @Override
-        public boolean retainAll(@NonNull Collection<?> c) {
-            return false;
-        }
-
-        @Override
-        public void clear() {
-
-        }
-    };
+    public static final BlockingQueue<Runnable> workQueue = new LinkedBlockingQueue<>();
 
     private ThreadPoolConfig() {
 
